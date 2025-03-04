af75fb58ab60915520d0a0a17cdbb13c945ef6a0
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-04 12:59:46 -0300

    Arrays aula 4

---
+++ src/Arrays/ArraysAula4.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class ArraysAula4 {
+    public static void main(String[] args) {
+
+        int[] n1 = new int[3];
+
+        int[] n2 = {1,2,3,4,5};
+
+        int[] n3 = new int[]{1,2,3,4,5,6};
+
+        for ( int n : n2){
+            System.out.println(n2[n]);
+        }
+
+    }
+}
