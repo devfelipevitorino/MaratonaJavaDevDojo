db40bba08a96c6396eba9ecfe5fc53696c435b96
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-06 10:06:05 -0300

    Classes aula 8

---
+++ src/_03OrientacaoObjetos/Classes/ClassesAula8/ClassesAula8.java
@@ -0,0 +1,19 @@
+package _03OrientacaoObjetos.Classes.ClassesAula8;
+
+import _03OrientacaoObjetos.Classes.ClassesAula8.Dominio.Pessoa;
+
+public class ClassesAula8 {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+
+        p1.nome = "Luffy";
+        p1.idade = 19;
+        p1.sexo = 'M';
+
+        System.out.println(p1.nome);
+        System.out.println(p1.idade);
+        System.out.println(p1.sexo);
+
+    }
+}
---
+++ src/_03OrientacaoObjetos/Classes/ClassesAula8/Dominio/Pessoa.java
@@ -0,0 +1,8 @@
+package _03OrientacaoObjetos.Classes.ClassesAula8.Dominio;
+
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+    public char sexo;
+}
