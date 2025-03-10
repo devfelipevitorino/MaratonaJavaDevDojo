fa3dac2b5ef9adf77aa3190686c87ec8c26542de
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-10 08:47:02 -0300

    Metodos aula 21

---
+++ src/_03OrientacaoObjetos/Metodos/MetodosAula21/Dominio/Funcionario.java
@@ -0,0 +1,33 @@
+package _03OrientacaoObjetos.Metodos.MetodosAula21.Dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salario;
+
+    public void imprimeDadosFuncionario(){
+        System.out.println("<-----------Dados--do--Funcionário------------->");
+        System.out.println("Nome: " +this.nome);
+        System.out.println("Idade: " +this.idade);
+        if(salario == null){
+            return;
+        }
+        System.out.println("<-----------Salários------------->");
+        for (double s : salario){
+            System.out.println("R$" + s);
+        }
+    }
+
+    public void MediaSalario(){
+        if (salario == null) {
+            return;
+        }
+        System.out.println("<-----------Média--Salarial----------->");
+        double media = 0;
+        for (double s : salario) {
+            media += s;
+        }
+        media /= salario.length;
+        System.out.printf("R$ %.2f%n", media);
+    }
+}
