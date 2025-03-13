0233f4dd6ad2516b90442ad7e1a0700619215e5d
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-12 23:02:21 -0300

    Construtores aula 27

---
+++ src/_03OrientacaoObjetos/_05Construtores/ContrutoresAula27/AnimeTest.java
@@ -0,0 +1,12 @@
+package _03OrientacaoObjetos._05Construtores.ContrutoresAula27;
+
+import _03OrientacaoObjetos._05Construtores.ContrutoresAula27.Dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Akudama Drive","TV",12,"Ação", "Production IG");
+
+        anime.imprime();
+
+    }
+}
