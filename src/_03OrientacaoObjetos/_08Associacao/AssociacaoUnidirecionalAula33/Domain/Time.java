a89d30b4cf8cf31492d5cf3fc5e6835c0cce74dc
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-15 16:17:42 -0300

    AssociacaoUnidirecioanal aula 33

---
+++ src/_03OrientacaoObjetos/_08Associacao/AssociacaoUnidirecionalAula33/Domain/Time.java
@@ -0,0 +1,21 @@
+package _03OrientacaoObjetos._08Associacao.AssociacaoUnidirecionalAula33.Domain;
+
+public class Time {
+
+    private String nome;
+
+
+    public Time(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
