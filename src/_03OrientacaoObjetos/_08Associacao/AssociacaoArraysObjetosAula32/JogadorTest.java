c8e2a8fa86200a9f94e38d4f35cc54979f9f947f
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-15 16:06:41 -0300

    AssociacaoArraysObjetos aula 32

---
+++ src/_03OrientacaoObjetos/_08Associacao/AssociacaoArraysObjetosAula32/JogadorTest.java
@@ -0,0 +1,19 @@
+package _03OrientacaoObjetos._08Associacao.AssociacaoArraysObjetosAula32;
+
+import _03OrientacaoObjetos._08Associacao.AssociacaoArraysObjetosAula32.Domain.Jogador;
+
+public class JogadorTest {
+    public static void main(String[] args) {
+
+        Jogador j1 = new Jogador("Ronaldo");
+        Jogador j2 = new Jogador("Romario");
+        Jogador j3 = new Jogador("Cafu");
+
+        Jogador[] jogadores = {j1,j2,j3};
+
+        for (Jogador jogador : jogadores ){
+            jogador.imprime();
+        }
+
+    }
+}
