a49eccf7dcad5019708648e51ac172a1fea95f53
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-16 12:47:27 -0300

    LeituraDadosConsole aula 36

---
+++ src/_03OrientacaoObjetos/_08Associacao/_8_1LeituraTeclado/LeituraTeclado.java
@@ -0,0 +1,23 @@
+package _03OrientacaoObjetos._08Associacao._8_1LeituraTeclado;
+
+import java.util.Scanner;
+
+public class LeituraTeclado {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome abaixo: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite sua idade abaixo: ");
+        int idade = scanner.nextInt();
+        System.out.println("Digite M ou F para seu sexo: ");
+        char sexo = scanner.next().charAt(0);
+        System.out.println("--------------------------------");
+        System.out.println("Nome: " +nome);
+        System.out.println("Idade: " +idade);
+        System.out.println("Sexo: " +sexo);
+
+
+    }
+}
