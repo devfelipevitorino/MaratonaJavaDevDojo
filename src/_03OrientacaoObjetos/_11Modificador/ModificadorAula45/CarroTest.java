4c25511b7bf71bc8b2ebde80547b2fff2c619c04
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-19 19:29:56 -0300

    Modificador final aula 45

---
+++ src/_03OrientacaoObjetos/_11Modificador/ModificadorAula45/CarroTest.java
@@ -0,0 +1,11 @@
+package _03OrientacaoObjetos._11Modificador.ModificadorAula45;
+
+import _03OrientacaoObjetos._11Modificador.ModificadorAula45.Domain.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
---
+++ src/_03OrientacaoObjetos/_11Modificador/ModificadorAula45/Domain/Carro.java
@@ -0,0 +1,14 @@
+package _03OrientacaoObjetos._11Modificador.ModificadorAula45.Domain;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
