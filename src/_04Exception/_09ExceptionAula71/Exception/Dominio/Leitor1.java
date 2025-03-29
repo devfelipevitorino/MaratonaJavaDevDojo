05ec728a5f59f8b750b2fbcaf91b6d4e12caa71d
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-29 17:47:03 -0300

    Exception aula 71

---
+++ src/_04Exception/_09ExceptionAula71/Exception/Dominio/Leitor1.java
@@ -0,0 +1,12 @@
+package _04Exception._09ExceptionAula71.Exception.Dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor1 implements Closeable {
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Fechando Leitor 1");
+    }
+}
