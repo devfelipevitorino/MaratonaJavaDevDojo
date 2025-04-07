374457967a5208e0fb5337d2cbfcbe6545cb626b
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-07 20:11:10 -0300

    SimpleDateFormatParse Aula 87

--- src/_05ClassesUtilitarias/_04Formatacao/_04SimpleDateFormatTestAula87.java
+++ src/_05ClassesUtilitarias/_04Formatacao/_04SimpleDateFormatTestAula87.java
@@ -1,5 +1,6 @@
 package _05ClassesUtilitarias._04Formatacao;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,6 +11,11 @@ public class _04SimpleDateFormatTestAula87 {
 
         SimpleDateFormat sdf = new SimpleDateFormat(mascara);
         System.out.println(sdf.format(new Date()));
+        try {
+            System.out.println(sdf.parse("Amsterdam 07 de abril de 2025"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
